c96247a2287a1eea864a80920eda9264a3b36b3f
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-04-20 17:45:34 +0200

    Auto-redirect unconfirmed pages when status changes

--- frontend/src/pages/UnconfirmedEmail.tsx
+++ frontend/src/pages/UnconfirmedEmail.tsx
@@ -1,13 +1,21 @@
 import { Button, Center, Stack, Title } from "@mantine/core";
 import { IconMail } from "@tabler/icons-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
 import { useSendConfirmationMail } from "../orval/generated/user/user";
 import IconTitle from "../components/IconTitle";
+import { useCurrentUser } from "../context/useCurrentUser";
 
 export default function UnconfirmedEmail() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { user } = useCurrentUser();
   const [emailSent, setEmailSent] = useState(false);
+
+  useEffect(() => {
+    if (user?.email_confirmed) navigate("/", { replace: true });
+  }, [user?.email_confirmed, navigate]);
 
   const {
     mutate: send,
--- frontend/src/pages/UnconfirmedUser.tsx
+++ frontend/src/pages/UnconfirmedUser.tsx
@@ -1,10 +1,19 @@
 import { Center, Stack } from "@mantine/core";
 import { IconClock } from "@tabler/icons-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
 import IconTitle from "../components/IconTitle";
+import { useCurrentUser } from "../context/useCurrentUser";
 
 export default function UnconfirmedUser() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { user } = useCurrentUser();
+
+  useEffect(() => {
+    if (user?.user_confirmed) navigate("/", { replace: true });
+  }, [user?.user_confirmed, navigate]);
 
   return (
     <Center>
