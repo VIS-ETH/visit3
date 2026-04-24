c6ab0ea262dbd764fda56c98abcb77d44ba1f484
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-04-25 01:01:23 +0200

    Apply BaseEntity, BaseLink, BaseToken to user and company models

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -7,7 +7,9 @@ from sqlmodel import Field, SQLModel
 
 class AppBase(SQLModel):
     created_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+        sa_column=Column(
+            DateTime(timezone=True), server_default=func.now(), nullable=False
+        )
     )
     deleted_at: datetime | None = Field(
         sa_column=Column(DateTime(timezone=True), nullable=True), default=None
@@ -28,3 +30,14 @@ class BaseEntity(AppBase):
 
 class BaseLink(AppBase):
     pass
+
+
+class BaseToken(BaseEntity):
+    user_id: UUID = Field(
+        foreign_key="user.id", ondelete="CASCADE", nullable=False, index=True
+    )
+    token: str = Field(index=True, unique=True)
+    is_revoked: bool = Field(default=False)
+    expires_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), nullable=False, index=True)
+    )
