1789593aeec3db1d1a6944885909704c3af94d50
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-04-25 00:50:23 +0200

    Add AppBase, BaseEntity, BaseLink model base classes

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
+from .base import *
 from .company import *
 from .kp_event import *
 from .user import *
---
+++ backend/app/models/base.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import Column, DateTime, func
+from sqlmodel import Field, SQLModel
+
+
+class AppBase(SQLModel):
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    )
+    deleted_at: datetime | None = Field(
+        sa_column=Column(DateTime(timezone=True), nullable=True), default=None
+    )
+
+
+class BaseEntity(AppBase):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    updated_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            server_default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+        )
+    )
+
+
+class BaseLink(AppBase):
+    pass
