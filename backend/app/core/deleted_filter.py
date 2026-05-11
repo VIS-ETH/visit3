b32d5c58aa7259caae5792c5dfcee99fcf28971d
Burak Emir Sezen <burakemirsezen@gmail.com>
2026-05-12 00:49:08 +0200

    Fix bug in soft delete

--- backend/app/core/deleted_filter.py
+++ backend/app/core/deleted_filter.py
@@ -1,8 +1,10 @@
+from collections.abc import Iterator
 from threading import Lock
 
 from sqlalchemy import event
 from sqlalchemy.orm import ORMExecuteState, Session, with_loader_criteria
 from sqlalchemy.sql.base import Executable
+from sqlmodel import SQLModel
 
 from app.models.base import AppBase
 
@@ -15,7 +17,15 @@ def include_deleted(statement: Executable) -> Executable:
     return statement.execution_options(**{INCLUDE_DELETED: True})
 
 
+def _model_subclasses(model: type[SQLModel]) -> Iterator[type[SQLModel]]:
+    for subclass in model.__subclasses__():
+        yield subclass
+        yield from _model_subclasses(subclass)
+
+
 def _exclude_deleted_rows(execute_state: ORMExecuteState) -> None:
+    """Exclude rows where deleted_at is not null from all ORM queries by default
+    i.e. exclude deleted rows from query results"""
     if (
         not execute_state.is_select
         or execute_state.is_column_load
@@ -25,10 +35,14 @@ def _exclude_deleted_rows(execute_state: ORMExecuteState) -> None:
         return
 
     execute_state.statement = execute_state.statement.options(
-        with_loader_criteria(
-            AppBase,
-            lambda cls: cls.deleted_at.is_(None),
-            include_aliases=True,
+        *(
+            with_loader_criteria(
+                model,
+                lambda cls: cls.deleted_at.is_(None),
+                include_aliases=True,
+            )
+            for model in _model_subclasses(AppBase)
+            if hasattr(model, "deleted_at")
         )
     )
 
