3de44db453b00ffa00b2fcc5314818a20beaec4b
lxorb <101118850+lxorb@users.noreply.github.com>
2026-09-24 22:48:46 +0200

    tests: extend matchers per file

--- frontend/src/test/setupTests.ts
+++ frontend/src/test/setupTests.ts
@@ -1,6 +1,6 @@
-import "@testing-library/jest-dom/vitest";
+import * as jestDomMatchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll, vi } from "vitest";
+import { afterAll, afterEach, beforeAll, expect, vi } from "vitest";
 import { notificationsShow, resetNotificationsMock } from "./notifications";
 import { server } from "./server";
 import { testBackendUrl, testStaticBase, testVisWebsiteUrl } from "./constants";
@@ -71,6 +71,7 @@ const mountServerData = () => {
   document.head.appendChild(element);
 };
 
+expect.extend(jestDomMatchers);
 installBrowserApisMissingFromJsdom();
 mountOrgConfig();
 mountServerData();
